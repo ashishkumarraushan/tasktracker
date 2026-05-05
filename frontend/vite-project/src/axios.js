f03d791cad031954c5e057ecea5155f9ca36638c
ashish kumar raushan <ashishpratapsingh404@gmail.com>
2026-05-05 11:47:28 +0530

    Change API base URL to local development server

--- frontend/vite-project/src/axios.js
+++ frontend/vite-project/src/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: "https://tasktracker-backend-usnw.onrender.com"
+    baseURL: "http://localhost:5000/api"
 });
 
 // Request interceptor to add token
